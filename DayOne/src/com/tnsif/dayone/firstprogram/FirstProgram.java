42ef975f30f6d873a057f0b8c1ca6c2f7d6bcf8d
HP <HP@LAPTOP-REUQ2DKO>
2024-11-28 11:49:52 +0530

    Day one

---
+++ DayOne/src/com/tnsif/dayone/firstprogram/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.tnsif.dayone.firstprogram;
+
+public class FirstProgram {
+
+	
+	public static void main(String[] args) {
+	
+		System.out.println("-------Hello All---------- ");    //syso+ ctrl+ space + enter
+	}
+}
---
+++ Demo/src/com/tnsif/dayone/startprog/FirstProgram.java
@@ -0,0 +1,9 @@
+package com.tnsif.dayone.startprog;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hiiii");
+	}
+}
