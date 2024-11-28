46c8a8b278190190e9ff97e2b4ea2c0bec989854
HP <HP@LAPTOP-REUQ2DKO>
2024-11-28 11:58:54 +0530

    tyqFGHQVH

---
+++ Demo2/src/Fisrt.java
@@ -0,0 +1,12 @@
+
+public class Fisrt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hyfdghgdhjwgdhj");
+		System.out.println("12345678");
+		System.out.println("___________");
+	}
+
+}
